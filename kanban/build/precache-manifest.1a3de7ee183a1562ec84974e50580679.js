36f6fedbe4365b7bc81ff12d199e4fc3e405f400
ryuk156 <yp15601560@gmail.com>
2021-04-03 01:11:39 +0530

    deploy

---
+++ kanban/build/precache-manifest.1a3de7ee183a1562ec84974e50580679.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e878cdaeb5657336941076e6d5760cb0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6f808ea0bc61c358377e",
+    "url": "/static/css/main.c21c922f.chunk.css"
+  },
+  {
+    "revision": "b2b94cc41d853f413886",
+    "url": "/static/js/2.eb6e44ac.chunk.js"
+  },
+  {
+    "revision": "6a858d7a9a2ca7f2df6a07c7d43d632b",
+    "url": "/static/js/2.eb6e44ac.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6f808ea0bc61c358377e",
+    "url": "/static/js/main.8ba40638.chunk.js"
+  },
+  {
+    "revision": "06febe2b8fded59e243e",
+    "url": "/static/js/runtime-main.f85ec9e6.js"
+  },
+  {
+    "revision": "102eff72f4f77a772ade9a41666a0680",
+    "url": "/static/media/growth-2.102eff72.png"
+  },
+  {
+    "revision": "258dd8a5ee75a1bc6664920456cf8de6",
+    "url": "/static/media/login.258dd8a5.png"
+  },
+  {
+    "revision": "7e50f88aaf76aef49fd134ab20966190",
+    "url": "/static/media/plan.7e50f88a.png"
+  }
+]);
